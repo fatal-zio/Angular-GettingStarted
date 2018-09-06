1f2e67584b159f7ae8183eed1f9e3c42511fe091
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2018-09-06 11:06:20 -0500

    Added Product Service.

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -25,53 +26,9 @@ export class ProductListComponent implements OnInit {
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-          },
-          {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-          },
-          {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-          },
-          {
-            "productId": 17,
-            "productName": "Ninja",
-            "productCode": "NJX-6746",
-            "releaseDate": "May 1, 2018",
-            "description": "Cute ninja",
-            "price": 49.99,
-            "starRating": 4.4,
-            "imageUrl": "http://media.openclipart.org/people/GDJ/50px-1535162072.png"
-          }  
-    ];
+    products: IProduct[];
 
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
-    }
+    constructor(private productService: ProductService) {}
 
     performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
@@ -85,7 +42,8 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit() : void {
-        console.log('In OnInit');
+        this.products = this.productService.getProducts();        
+        this.filteredProducts = this.products;
     }
 
     onRatingClicked(message: string) : void {

6a21d972efd58d94f6e17beaf22c4e35451eb074
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2018-09-07 10:32:07 -0500

    Added basic routing to and from the Product Detail component.

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,9 +10,25 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Dettail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) {  }
 
   ngOnInit() {
+    //this plus sign is a casting operator to cast from string to numeric.
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      "productId": 17,
+      "productName": "Ninja",
+      "productCode": "NJX-6746",
+      "releaseDate": "May 1, 2018",
+      "description": "Cute ninja",
+      "price": 49.99,
+      "starRating": 4.4,
+      "imageUrl": "http://media.openclipart.org/people/GDJ/50px-1535162072.png"
+    }
   }
 
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
